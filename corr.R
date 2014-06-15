fd96e14178a1c1195de495063a3339fd3d0353f3
tglunde <t.glunde@alligator-company.de>
2014-06-16 01:59:59 +0200

    finish
    finish

--- corr.R
+++ corr.R
@@ -1,19 +1,13 @@
 corr <- function(directory, threshold=0) {
   files <- list.files(directory, full=TRUE)
-  print(files)
+  correlation <- numeric(0)
   for(filename in files) {
     nobs<-0
     tempset <- read.csv(filename, header=TRUE)
     nobs <- sum(!is.na(tempset["sulfate"]) & !is.na(tempset["nitrate"]))
-    print(paste(filename,nobs,sep=": "))
-    if(nobs>threshold) {
-      tempcor <- cor(tempset["sulfate"],tempset["nitrate"])
-      if(!exists("dataset")) {
-        correlation <- tempcor
-      } else {
-        correlation<-rbind(correlation, tempcor)
-      }
-      rm(tempcor)
+    if(nobs>=threshold) {
+      p <- tempset[complete.cases(tempset),]
+      correlation<-append(correlation, cor(p$sulfate,p$nitrate))
     }
     rm(tempset)
   }
